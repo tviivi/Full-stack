5c92abb2003906b58105ff44f661738abc9f950c
tviivi <viivi.tiihonen@helsinki.fi>
2018-02-02 15:54:37 +0200

    tehtävä 2.14

---
+++ viikko2/2.14-/src/components/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Person =  ({person}) => {
+  return (
+    <tr>
+      <td>{person.name}</td>
+      <td>{person.number}</td>
+    </tr>
+  )
+}
+
+export default Person
